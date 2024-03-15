c8d7f01d32ce778f6d17c735c273f40c69186486
LarsGJobloop <lars.gunnar@jobloop.no>
2024-03-15 12:02:26 +0100

    setup step change function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-prompt("Hello World")
+/**
+ * Lage en funksjon som kan bytte
+ * CSS klassen på hoved form elementet
+ */
+function changeStep() {
+    // Vi må finne dette html elementet
+    const formElement = document.getElementById("multi-step-form")
+
+    // Først fjern den gamle klassen
+    formElement.classList.remove("current-step-1")
+    formElement.classList.remove("current-step-2")
+    formElement.classList.remove("current-step-3")
+    formElement.classList.remove("current-step-4")
+
+    // Legg til den klassen vi ønsker
+    formElement.classList.add("current-step-4")
+}
